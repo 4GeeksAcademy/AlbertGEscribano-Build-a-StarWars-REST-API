bcf2fe96f15b77ae5a35fe88dd825698e122a52d
Albert González Escribano <algoes2002@gmail.com>
2023-07-12 23:30:31 +0000

    First commit

--- src/admin.py
+++ src/admin.py
@@ -1,7 +1,10 @@
 import os
 from flask_admin import Admin
-from models import db, User
+
 from flask_admin.contrib.sqla import ModelView
+
+from models import db, User, Address, Character, Planet, Vehicle, FavoriteList
+
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
@@ -14,3 +17,19 @@ def setup_admin(app):
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
+
+    
+    # Add Address model to the admin
+    admin.add_view(ModelView(Address, db.session))
+
+     # Add Planet model to the admin
+    admin.add_view(ModelView(Planet, db.session))
+
+    # Add Character model to the admin
+    admin.add_view(ModelView(Character, db.session))
+
+    # Add Vehicle model to the admin
+    admin.add_view(ModelView(Vehicle, db.session))
+
+    # Add FavoriteList model to the admin
+    admin.add_view(ModelView(FavoriteList, db.session))
